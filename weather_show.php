c34735deabd34f9c6c4fcefd18caf1d95562ee26
Skolnieks <skolnieks@vtdt.edu.lv>
2025-09-02 11:25:37 +0300

    city name

--- weather_show.php
+++ weather_show.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Weather app</title>
 </head>
 <body>
  <?php 
  $data = file_get_contents("https://emo.lv/weather-api/forecast/?
  city=cesis,latvia");
  $weatherData = json_decode($data, true);
- echo "Pilsēta: " . $weatherData['location']['city'];
+ echo "Pilsēta: " . $weatherData['city']['name'];
  
  ?>
 </body>
