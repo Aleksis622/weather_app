1855738945e19687b94f00a64b5f2a57ce20fe06
Aleksis622 <ipb23.a.eglitis@vtdt.edu.lv>
2025-09-07 15:41:34 +0300

    json dati ir dabuti

--- weather_show.php
+++ weather_show.php
@@ -1,17 +1,49 @@
+<?php 
+
+$jsonData = file_get_contents("data.json");
+$data = json_decode($jsonData);
+
+// info par pilsētu
+
+$city = $data->city->name;
+$country = $data->city->country;
+$timezone = $data->city->timezone;
+
+$current = $data->list[0];
+$weather = $current->weather[0];
+
+// laikapstākļu dati
+
+$localtime = gmdate("H:i A", $current->dt +$timezone);
+$temp = $current->temp->day;
+$feelsLike = $current->feels_like->day;
+$weatherMain = $weather->main;
+$weatherDesc = $weather->description;
+$icon = $weather->icon;
+$wind = $current->speed;
+$pressure = $current->pressure;
+$humidity = $current->humidity;
+
+
+
+
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather app</title>
+  <link rel="stylesheet" href="weather.css">
 </head>
 <body>
- <?php 
- $data = file_get_contents("https://emo.lv/weather-api/forecast/?
- city=cesis,latvia");
- $weatherData = json_decode($data, true);
- echo "Pilsēta: " . $weatherData['city']['name'];
- 
- ?>
+
 </body>
 </html>
