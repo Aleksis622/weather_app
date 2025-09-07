492d8439aad910d21b540c55a4cd06dbcea59eb8
Aleksis622 <ipb23.a.eglitis@vtdt.edu.lv>
2025-09-07 19:05:12 +0300

    added permanent style to project

--- weather_show.php
+++ weather_show.php
@@ -44,6 +44,14 @@ $humidity = $current->humidity;
   <link rel="stylesheet" href="weather.css">
 </head>
 <body>
-
+<h1>Weather in <?php echo "$city, $country"; ?></h1>
+  <p><strong>Local Time:</strong> <?php echo $localtime; ?></p>
+  <p><strong>Temperature:</strong> <?php echo $temp; ?> °C</p>
+  <p><strong>Feels Like:</strong> <?php echo $feelsLike; ?> °C</p>
+  <p><strong>Condition:</strong> <?php echo $weatherMain; ?> (<?php echo $weatherDesc; ?>)</p>
+  <p><strong>Wind Speed:</strong> <?php echo $wind; ?> m/s</p>
+  <p><strong>Pressure:</strong> <?php echo $pressure; ?> hPa</p>
+  <p><strong>Humidity:</strong> <?php echo $humidity; ?> %</p>
+  <p><strong>Icon:</strong> <img src="http://openweathermap.org/img/wn/<?php echo $icon; ?>@2x.png" alt="Weather icon"></p>
 </body>
 </html>
